37ec4825983e171523f4b3c38e2b72562c47bf1e
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-09-18 19:43:56 +0200

    fix(backend>common>filters): Fixed type specifications

--- backend/src/common/filters/http-exception.filter.ts
+++ backend/src/common/filters/http-exception.filter.ts
@@ -1,17 +1,19 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
 import { Response } from 'express';
+import {HttpArgumentsHost} from "@nestjs/common/interfaces";
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
-        const ctx = host.switchToHttp();
-        const response = ctx.getResponse<Response>();
-        const status = exception.getStatus();
+        const ctx: HttpArgumentsHost = host.switchToHttp();
+        const response: Response = ctx.getResponse<Response>();
+        const status: number = exception.getStatus();
+        const now: string = new Date().toISOString()
 
         response.status(status).json({
             statusCode: status,
             message: exception.message,
-            timestamp: new Date().toISOString(),
+            timestamp: now,
         });
     }
 }
