54c6ba3c464a002f21f989c36053697818094871
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-09-18 16:25:49 +0200

    chore: Added bare backend nest architecture

---
+++ backend/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
+
+async function bootstrap() {
+
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.setGlobalPrefix('api')
+
+    await app.listen(process.env.PORT ?? 3000);
+
+}
+
+bootstrap();
