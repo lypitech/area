9c19f6783fbb895f87ea7f93b06c9b1d5abf54ee
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-09-25 16:06:42 +0200

    feat(front>mobile>presentation): Added register page dispatcher.

---
+++ frontend/mobile/lib/presentation/auth/registration/register_page.dart
@@ -0,0 +1,28 @@
+import 'package:area/data/provider/register_modal_provider.dart';
+import 'package:area/presentation/auth/registration/step/register_page_step_1.dart';
+import 'package:area/presentation/auth/registration/step/register_page_step_2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class RegisterPage extends ConsumerWidget {
+
+  const RegisterPage({
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final registerModal = ref.watch(registerModalProvider);
+
+    return switch (registerModal.currentPage) {
+      0 => RegisterPageStep1(),
+      1 => RegisterPageStep2(),
+      2 => RegisterPageStep1(),
+      3 => RegisterPageStep1(),
+      4 => RegisterPageStep1(),
+      5 => RegisterPageStep1(),
+      int() => throw UnimplementedError(),
+    };
+  }
+
+}
