24699b08b9d30636d8648d72842b7accb19d6267
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-09-30 20:50:39 +0200

    refactor/feat(front>mobile>presentation>new_area): Now uses `AreactionCard`.

--- frontend/mobile/lib/presentation/new_area/new_area_page.dart
+++ frontend/mobile/lib/presentation/new_area/new_area_page.dart
@@ -1,7 +1,7 @@
 import 'package:area/data/provider/area_modal_provider.dart';
 import 'package:area/layout/main_page_layout.dart';
 import 'package:area/widget/appbar_button.dart';
-import 'package:area/widget/clickable_frame.dart';
+import 'package:area/widget/areaction_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -26,47 +26,16 @@ class NewAreaPage extends ConsumerWidget {
         }
       ),
       children: [
-        ClickableFrame(
-          padding: const EdgeInsets.all(20),
-          child: Row(
-            spacing: 10,
-            children: [
-              Container(
-                height: 64,
-                width: 64,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                ),
-                child: Placeholder(), // TODO: Put platform icon
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      areaModal.actionPlatform != null
-                        ? areaModal.actionPlatform!.name
-                        : 'Choose a platform',
-                      style: textTheme.titleSmall?.copyWith(
-                        fontWeight: FontWeight.w300
-                      ),
-                    ),
-                    Text(
-                      areaModal.trigger != null
-                        ? areaModal.trigger!.name
-                        : 'Choose a trigger',
-                      style: textTheme.titleMedium?.copyWith(
-                        fontSize: 20
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Icon(
-                Icons.arrow_forward_ios_rounded
-              )
-            ],
-          )
+        AreactionCard(
+          title: areaModal.trigger != null
+            ? areaModal.trigger!.name
+            : 'Choose a trigger',
+          subtitle: areaModal.actionPlatform != null
+            ? areaModal.actionPlatform!.name
+            : 'Choose a platform',
+          onTap: () {
+            // ...
+          }
         ),
         Column(
           spacing: 5,
@@ -85,12 +54,23 @@ class NewAreaPage extends ConsumerWidget {
               ),
             ),
             Text(
-              '... then do',
+              'Then',
               style: textTheme.titleLarge?.copyWith(
                 fontWeight: FontWeight.w500
               ),
             )
           ],
+        ),
+        AreactionCard(
+          title: areaModal.action != null
+            ? areaModal.action!.name
+            : 'Choose an action',
+          subtitle: areaModal.reactionPlatform != null
+            ? areaModal.reactionPlatform!.name
+            : 'Choose a platform',
+          onTap: () {
+            // ...
+          }
         ),
       ]
     );
