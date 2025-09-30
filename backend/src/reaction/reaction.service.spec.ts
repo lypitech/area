ed834f0ec5d220799628b7eff2c77e7801915ae5
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-09-30 16:10:51 +0200

    style: Added line break and removing merge conflict residues

--- backend/src/reaction/reaction.service.spec.ts
+++ backend/src/reaction/reaction.service.spec.ts
@@ -1,17 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-<<<<<<< HEAD
-import { ReactionsService } from './reaction.service';
-
-describe('ReactionsService', () => {
-  let service: ReactionsService;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ReactionsService],
-    }).compile();
-
-    service = module.get<ReactionsService>(ReactionsService);
-=======
 import { ReactionService } from './reaction.service';
 
 describe('ReactionService', () => {
@@ -23,7 +10,6 @@ describe('ReactionService', () => {
     }).compile();
 
     service = module.get<ReactionService>(ReactionService);
->>>>>>> 2e84b49 (feat(backend>reaction): Added `Reaction` module setup)
   });
 
   it('should be defined', () => {
