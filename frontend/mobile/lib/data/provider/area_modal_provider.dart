4ca19dd0f3ed4a3bc23a32cc939b31d1c4052320
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-09-30 20:19:39 +0200

    feat(front>mobile>data>provider>area_modal): Now automatically disposes (for "drafts").

--- frontend/mobile/lib/data/provider/area_modal_provider.dart
+++ frontend/mobile/lib/data/provider/area_modal_provider.dart
@@ -1,6 +1,7 @@
 import 'package:area/modal/area_modal.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
-final areaModalProvider = StateNotifierProvider<AreaModalNotifier, AreaModal>((ref) {
-  return AreaModalNotifier();
-});
+final areaModalProvider =
+  StateNotifierProvider.autoDispose<AreaModalNotifier, AreaModal>((ref) {
+    return AreaModalNotifier();
+  });
