298caaf6fababf2d315ef424b687f0f1007ea882
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-10-02 15:53:33 +0200

    feat(backend>actionSelection): Added `ActionSelection` CRUD

--- backend/src/action/action.service.ts
+++ backend/src/action/action.service.ts
@@ -2,11 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Action, ActionDocument } from './schemas/action.schemas';
+import {
+  ActionSelection,
+  ActionSelectionType,
+} from './schemas/actionSelection.schema';
 
 @Injectable()
 export class ActionService {
   constructor(
     @InjectModel(Action.name) private actionModel: Model<ActionDocument>,
+    @InjectModel(ActionSelection.name)
+    private actionSelectionModel: Model<ActionSelection>,
   ) {}
 
   async getAll() {
@@ -24,5 +30,22 @@ export class ActionService {
 
   async remove(uuid: string): Promise<Action | null> {
     return this.actionModel.findOneAndDelete({ uuid }).exec();
+  }
+
+  getAllSelection() {
+    return this.actionSelectionModel.find().exec();
+  }
+
+  getSelectionByUUID(uuid: string) {
+    return this.actionSelectionModel.findOne({ uuid: uuid }).exec();
+  }
+
+  createActionSelection(data: ActionSelectionType) {
+    const newActionSelection = new this.actionSelectionModel(data);
+    return newActionSelection.save();
+  }
+
+  async removeSelection(uuid: string): Promise<ActionSelection | null> {
+    return this.actionSelectionModel.findOneAndDelete({ uuid: uuid }).exec();
   }
 }
