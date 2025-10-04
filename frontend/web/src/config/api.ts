9ce262fc7021a17e601a1c8b7d0d408fb489ddd3
Babouye <esteban.bouyault-yvanez@epitech.eu>
2025-10-04 16:45:22 +0200

    feat(front>web>config): switch port 3000 to 8080 for api base url and add refresh route

--- frontend/web/src/config/api.ts
+++ frontend/web/src/config/api.ts
@@ -1,8 +1,9 @@
-export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "http://localhost:8080";
 
 export const API_ROUTES = {
   auth: {
     register: `${API_BASE_URL}/login/register`,
     login: `${API_BASE_URL}/login`,
+    refresh: `${API_BASE_URL}/login/refresh`,
   },
 };
