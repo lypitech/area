bfb2e1f45a7bdacb563bcb8adfb244f3720f7dfd
Babouye <esteban.bouyault-yvanez@epitech.eu>
2025-10-04 16:51:24 +0200

    feat(front>web>authServices): Added refreshToken api call function

--- frontend/web/src/services/authServices.ts
+++ frontend/web/src/services/authServices.ts
@@ -11,7 +11,7 @@ export async function register(
   const response = await fetch(API_ROUTES.auth.register, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password, name: nickname, username, profile_picture }),
+    body: JSON.stringify({ email, password, nickname, username, profile_picture }),
   });
 
   if (!response.ok) {
@@ -35,4 +35,21 @@ export async function login(email: string, password: string) {
   }
 
   return response.json();
+}
+
+export async function refreshToken() {
+  const refresh_token = localStorage.getItem("refresh_token");
+  if (!refresh_token) throw new Error("No refresh token");
+
+  const res = await fetch("/login/refresh", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ refresh_token }),
+  });
+
+  if (!res.ok) throw new Error("Failed to refresh token");
+  const data = await res.json();
+
+  localStorage.setItem("access_token", data.access_token);
+  return data.access_token;
 }
