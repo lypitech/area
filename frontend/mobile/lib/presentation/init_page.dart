a2b7bc1d9d5454ba52ce2e0e2a31a4559395448e
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-06 16:09:43 +0200

    feat(front>mobile>presentation): Created Initialization page for auth management.

---
+++ frontend/mobile/lib/presentation/init_page.dart
@@ -0,0 +1,76 @@
+import 'package:area/data/provider/auth_provider.dart';
+import 'package:area/widget/logo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class InitPage extends ConsumerStatefulWidget {
+
+  const InitPage({
+    super.key
+  });
+
+  @override
+  ConsumerState<InitPage> createState() => _InitPageState();
+
+}
+
+class _InitPageState extends ConsumerState<InitPage> {
+
+  bool _initialized = false;
+
+  @override
+  void initState() {
+    _init();
+    super.initState();
+  }
+
+  Future<void> _init() async {
+    if (_initialized) {
+      return;
+    }
+
+    _initialized = true;
+
+    final authNotifier = ref.read(authNotifierProvider.notifier);
+    await authNotifier.checkAuth();
+
+    final authState = ref.read(authNotifierProvider);
+
+    if (!mounted) {
+      return;
+    }
+
+    switch (authState.status) {
+      case AuthState.authenticated:
+        context.replace('/');
+        break;
+      case AuthState.unauthenticated:
+        // context.go('/login');
+        // break;
+      case AuthState.unknown:
+      default:
+        context.replace('/login');
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            spacing: 20,
+            children: [
+              Logo(),
+              CircularProgressIndicator()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
