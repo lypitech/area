efda702313930ce9911b17ffbbb844416b76b31e
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-10-07 12:42:20 +0200

    ref(backend>list>reaction): Removed `Selection` feature

--- backend/src/reaction/reaction.module.ts
+++ backend/src/reaction/reaction.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
-import {
-  ReactionSelection,
-  ReactionSelectionSchema,
-} from './schemas/reactionSelection.schema';
 import { HttpModule } from '@nestjs/axios';
 import { ReactionController } from './reaction.controller';
 import { ReactionService } from './reaction.service';
 import { Reaction, ReactionSchema } from './schemas/reaction.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DiscordReactionService } from './services/discord.service';
-import { SelectionController } from './selection/selection.controller';
-import { SelectionService } from './selection/selection.service';
 
 @Module({
   imports: [
@@ -18,12 +12,9 @@ import { SelectionService } from './selection/selection.service';
     MongooseModule.forFeature([
       { name: Reaction.name, schema: ReactionSchema },
     ]),
-    MongooseModule.forFeature([
-      { name: ReactionSelection.name, schema: ReactionSelectionSchema },
-    ]),
   ],
-  controllers: [ReactionController, SelectionController],
-  providers: [ReactionService, DiscordReactionService, SelectionService],
+  controllers: [ReactionController],
+  providers: [ReactionService, DiscordReactionService],
   exports: [ReactionService],
 })
 export class ReactionModule {}
