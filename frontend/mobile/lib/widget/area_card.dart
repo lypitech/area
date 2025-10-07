ecc559694a713dc6149242a986ceb9a9ed49269f
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-07 17:09:02 +0200

    feat(front>mobile>widget): Created `AreaCard` widget.

---
+++ frontend/mobile/lib/widget/area_card.dart
@@ -0,0 +1,58 @@
+import 'package:area/model/area_model.dart';
+import 'package:area/widget/clickable_frame.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AreaCard extends StatelessWidget {
+
+  final AreaModel area;
+
+  const AreaCard({
+    required this.area,
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return ClickableFrame(
+      onTap: () {
+        context.pushNamed(
+          'area_details',
+          extra: area
+        );
+      },
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        spacing: 10,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            spacing: 5,
+            children: [
+              SizedBox(
+                height: 24,
+                width: 24,
+                child: Placeholder(),
+              ),
+              Icon(Icons.link_rounded),
+              SizedBox(
+                height: 24,
+                width: 24,
+                child: Placeholder(),
+              )
+            ],
+          ),
+          Text(
+            area.title,
+            style: textTheme.titleLarge?.copyWith(
+              fontWeight: FontWeight.w500
+            ),
+          )
+        ],
+      )
+    );
+  }
+
+}
