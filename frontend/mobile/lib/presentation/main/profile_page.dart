c69374953d78b601fc7e86f9afabb6dcbefde14e
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-07 20:45:01 +0200

    feat(front>mobile>presentation>profile_page): Drafted Github OAuth connection. Not working for now!
    # Conflicts:
    #	frontend/mobile/lib/presentation/main/profile_page.dart

--- frontend/mobile/lib/presentation/main/profile_page.dart
+++ frontend/mobile/lib/presentation/main/profile_page.dart
@@ -1,8 +1,11 @@
 import 'package:area/data/provider/auth_provider.dart';
+import 'package:area/data/provider/oauth/oauth_github_provider.dart';
 import 'package:area/l10n/app_localizations.dart';
 import 'package:area/layout/main_page_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:github_oauth/github_oauth.dart';
 import 'package:go_router/go_router.dart';
 
 class ProfilePage extends ConsumerWidget {
@@ -28,6 +31,26 @@ class ProfilePage extends ConsumerWidget {
     return MainPageLayout(
       title: l10n.profile_page_title,
       children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Github'),
+            Text('Status: UNKNOWN')
+          ],
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            final githubOAuth = ref.read(githubOAuthProvider);
+            final res = await githubOAuth.signIn(context);
+
+            if (res.status == GitHubSignInResultStatus.ok) {
+              Fluttertoast.showToast(msg: res.token.toString());
+            } else {
+              Fluttertoast.showToast(msg: 'Failed to link.\n${res.errorMessage}');
+            }
+          },
+          child: Text('Link account')
+        ),
         Text("Username: ${user.username}"),
         Text("Nickname: ${user.nickname}"),
         Text("Email: ${user.email}"),
