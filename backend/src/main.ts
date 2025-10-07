7918f7ae4ba3cb727c942b9257f62e8318b972a8
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-10-07 11:52:16 +0200

    feat(backend>login>swagger): Added login swagger doc

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as bodyParser from 'body-parser';
+import { User } from './user/schemas/user.schema';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,7 +22,9 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('area')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, { extraModels: [User] });
+
   SwaggerModule.setup('api', app, documentFactory);
 
   app.use('/action/github', bodyParser.raw({ type: '*/*' }));
