83b3b354305fe95b5936c78537d95aea005acbcc
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-10-17 11:42:03 +0200

    feat(backend>oauth): Added github oauth validation

--- backend/src/oauth/oauth.controller.ts
+++ backend/src/oauth/oauth.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { OauthService } from './oauth.service';
+import { GithubOauthCreationDto } from './types/githubOauthCreationDto';
 
 @Controller('oauth')
 export class OauthController {
   constructor(private readonly authService: OauthService) {}
 
   @Post('github')
-  async githubAuth(@Body('code') code: string, @Body('uuid') uuid: string) {
-    return this.authService.getGithubToken(code, uuid);
+  @UsePipes(new ValidationPipe())
+  async githubAuth(@Body() data: GithubOauthCreationDto) {
+    return this.authService.getGithubToken(data.code, data.uuid);
   }
 }
