dbd75dba2ffa6e0d19a8eee12b84658d85ad8e08
Timeo tregarot <timeo.tregarot@epitech.eu>
2025-10-21 09:31:29 +0200

    fix(backend>area): fix interfaces into area.service

--- backend/src/trigger/services/github/github.module.ts
+++ backend/src/trigger/services/github/github.module.ts
@@ -5,12 +5,14 @@ import { User, UserSchema } from 'src/user/schemas/user.schema';
 import { Trigger, TriggerSchema } from 'src/trigger/schemas/trigger.schema';
 import { GithubWebhookTriggerDriver } from './github.driver';
 import { GithubService } from './github.service';
+import { Oauth, OauthSchema } from 'src/oauth/schema/Oauth.schema';
 
 @Module({
   imports: [
     ConfigModule,
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     MongooseModule.forFeature([{ name: Trigger.name, schema: TriggerSchema }]),
+    MongooseModule.forFeature([{ name: Oauth.name, schema: OauthSchema }]),
   ],
   providers: [GithubService, GithubWebhookTriggerDriver],
   exports: [GithubWebhookTriggerDriver, GithubService],
