5955f56ea928a85ccfabd290398c1724d3aa3323
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-21 18:12:08 +0200

    feat(front>mobile>presentation>profile_page): Basically finished profile page. Some things are still under work though.

--- frontend/mobile/lib/presentation/main/profile_page.dart
+++ frontend/mobile/lib/presentation/main/profile_page.dart
@@ -2,8 +2,11 @@ import 'package:area/data/provider/areas_provider.dart';
 import 'package:area/data/provider/auth_provider.dart';
 import 'package:area/l10n/app_localizations.dart';
 import 'package:area/layout/main_page_layout.dart';
+import 'package:area/widget/a_card.dart';
+import 'package:area/widget/clickable_frame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 
 class ProfilePage extends ConsumerWidget {
@@ -26,17 +29,72 @@ class ProfilePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final l10n = AppLocalizations.of(context)!;
     final user = ref.watch(authNotifierProvider.notifier).getUser()!;
+    final textTheme = Theme.of(context).textTheme;
 
     return MainPageLayout(
-      title: l10n.profile_page_title,
       children: [
-        Text("Username: ${user.username}"),
-        Text("Nickname: ${user.nickname}"),
-        Text("Email: ${user.email}"),
-        Text("UUID: ${user.uuid}"),
-        ElevatedButton(
-          onPressed: () async => _logout(context, ref),
-          child: Text('Log out')
+        Column(
+          children: [
+            Container(
+              height: 128,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  fit: BoxFit.contain,
+                  image: AssetImage('assets/image/default_profile_picture.jpg')
+                ),
+              ),
+            ),
+            Gap(10),
+            Text(
+              user.nickname,
+              style: textTheme.titleLarge?.copyWith(
+                fontWeight: FontWeight.w600
+              ),
+            ),
+            Gap(5),
+            Text(
+              user.username,
+              style: textTheme.titleMedium?.copyWith(
+                fontWeight: FontWeight.w300,
+                color: Colors.grey.shade400
+              ),
+            ),
+            Gap(10),
+            ClickableFrame(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              onTap: () {
+                // ...
+              },
+              child: Text('Edit profile')
+            ),
+          ],
+        ),
+        ACard(
+          title: 'Informations',
+          children: [
+            Text("Email: ${user.email}")
+          ],
+        ),
+        ClickableFrame(
+          color: Colors.red,
+          onTap: () async => _logout(context, ref),
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+          child: Center(
+            child: Text(
+              'Log out',
+              style: textTheme.titleMedium?.copyWith(
+                color: Colors.white
+              ),
+            ),
+          ),
+        ),
+        Text(
+          'Account UUID\n${user.uuid}',
+          style: textTheme.bodyMedium?.copyWith(
+            color: Colors.grey.shade400
+          ),
+          textAlign: TextAlign.center,
         )
       ],
     );
