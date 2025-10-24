7f343222bf233b5da116c8367e75177f6fd498b6
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-24 15:32:14 +0200

    feat(front>mobile): API URL and port are now properly stored.

--- frontend/mobile/lib/main.dart
+++ frontend/mobile/lib/main.dart
@@ -1,19 +1,37 @@
+import 'package:area/core/constant/constants.dart';
 import 'package:area/data/provider/hive_box_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'app.dart';
+
+Future<bool> initAppSettings() async {
+  if (!dotenv.env.containsKey('API_URL') || !dotenv.env.containsKey('API_PORT')) {
+    print('This build is broken. Default API URL and port are not specified.');
+    return false;
+  }
+
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  if (!sharedPreferences.containsKey(Constants.apiUrlKey)) {
+    sharedPreferences.setString(Constants.apiUrlKey, dotenv.env['API_URL']!);
+  }
+
+  if (!sharedPreferences.containsKey(Constants.apiPortKey)) {
+    sharedPreferences.setString(Constants.apiPortKey, dotenv.env['API_PORT']!);
+  }
+
+  return true;
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: '.env');
-  if (!dotenv.env.containsKey('API_URL') || !dotenv.env.containsKey('API_PORT')) {
-    String errorMessage = 'Please specify the following fields in the environment variables (.env file):';
-    errorMessage += '\n- API_URL';
-    errorMessage += '\n- API_PORT';
-    print(errorMessage);
+
+  if (!(await initAppSettings())) {
     return;
   }
 
