e83010f3df2b32c29cae005b1ec804a10e382c03
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-25 15:47:31 +0200

    feat(front>mobile>presentation>auth>login_page): Added button in top left corner to change app settings (api url and api port).

--- frontend/mobile/lib/presentation/auth/login_page.dart
+++ frontend/mobile/lib/presentation/auth/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:area/core/constant/constants.dart';
 import 'package:area/core/constant/regexes.dart';
 import 'package:area/data/provider/auth_provider.dart';
+import 'package:area/presentation/dialog/app_settings_dialog.dart';
 import 'package:area/widget/a_text_field.dart';
 import 'package:area/widget/clickable_frame.dart';
 import 'package:area/widget/logo.dart';
@@ -53,150 +54,165 @@ class LoginPage extends ConsumerWidget {
     return Form(
       key: _authFormKey,
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsetsGeometry.all(20),
-          child: Center(
-            child: SingleChildScrollView(
-              child: Flex(
-                direction: Axis.vertical,
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Logo(),
-                  Gap(10),
-                  Text(
-                    Constants.appName,
-                    style: textTheme.displayMedium?.copyWith(
-                      fontWeight: FontWeight.w700
-                    ),
-                  ),
-                  Gap(40),
-                  ATextField(
-                    title: l10n.email,
-                    hintText: 'john.doe@example.com',
-                    leadingIcon: Icons.email_rounded,
-                    keyboardType: TextInputType.emailAddress,
-                    controller: _emailFieldController,
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return l10n.validator_empty_email;
-                      }
-                      if (!Regexes.email.hasMatch(value)) {
-                        return l10n.validator_invalid_email;
-                      }
-                      return null;
-                    },
-                  ),
-                  Gap(20),
-                  ATextField(
-                    title: l10n.password,
-                    leadingIcon: Icons.lock_rounded,
-                    obscureToggle: true,
-                    keyboardType: TextInputType.visiblePassword,
-                    controller: _passwordFieldController,
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return l10n.validator_empty_password;
-                      }
-                      return null;
-                    },
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          // ...
-                        },
-                        child: Text(
-                          l10n.forgot_password,
-                          textAlign: TextAlign.right,
-                          style: textTheme.bodyMedium?.copyWith(
-                            decoration: TextDecoration.underline
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Padding(
+                padding: const EdgeInsetsGeometry.all(20),
+                child: Center(
+                  child: SingleChildScrollView(
+                    child: Flex(
+                      direction: Axis.vertical,
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Logo(),
+                        Gap(10),
+                        Text(
+                          Constants.appName,
+                          style: textTheme.displayMedium?.copyWith(
+                            fontWeight: FontWeight.w700
                           ),
+                        ),
+                        Gap(40),
+                        ATextField(
+                          title: l10n.email,
+                          hintText: 'john.doe@example.com',
+                          leadingIcon: Icons.email_rounded,
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _emailFieldController,
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return l10n.validator_empty_email;
+                            }
+                            if (!Regexes.email.hasMatch(value)) {
+                              return l10n.validator_invalid_email;
+                            }
+                            return null;
+                          },
+                        ),
+                        Gap(20),
+                        ATextField(
+                          title: l10n.password,
+                          leadingIcon: Icons.lock_rounded,
+                          obscureToggle: true,
+                          keyboardType: TextInputType.visiblePassword,
+                          controller: _passwordFieldController,
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return l10n.validator_empty_password;
+                            }
+                            return null;
+                          },
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                // ...
+                              },
+                              child: Text(
+                                l10n.forgot_password,
+                                textAlign: TextAlign.right,
+                                style: textTheme.bodyMedium?.copyWith(
+                                  decoration: TextDecoration.underline
+                                ),
+                              )
+                            )
+                          ],
+                        ),
+                        Gap(40),
+                        ClickableFrame(
+                          onTap: () => _login(context, ref),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Icon(Icons.arrow_forward_ios_rounded),
+                          ),
+                        ),
+                        Gap(40),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          spacing: 7.5,
+                          children: [
+                            Container(
+                              width: 30,
+                              height: 2,
+                              color: Colors.black,
+                            ),
+                            Text(
+                              l10n.or_continue_with,
+                              style: textTheme.titleSmall,
+                            ),
+                            Container(
+                              width: 30,
+                              height: 2,
+                              color: Colors.black,
+                            )
+                          ],
+                        ),
+                        Gap(20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          spacing: 20,
+                          children: [
+                            ClickableFrame(
+                              child: Placeholder(
+                                fallbackWidth: 56,
+                                fallbackHeight: 56,
+                              ),
+                            ),
+                            ClickableFrame(
+                              child: Placeholder(
+                                fallbackWidth: 56,
+                                fallbackHeight: 56,
+                              ),
+                            ),
+                            ClickableFrame(
+                              child: Placeholder(
+                                fallbackWidth: 56,
+                                fallbackHeight: 56,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Gap(20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              l10n.register_prompt,
+                              style: textTheme.titleSmall?.copyWith(
+                                fontWeight: FontWeight.w300
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                context.push('/register');
+                              },
+                              child: Text(
+                                l10n.sign_up,
+                                style: textTheme.titleSmall,
+                              )
+                            )
+                          ],
                         )
-                      )
-                    ],
-                  ),
-                  Gap(40),
-                  ClickableFrame(
-                    onTap: () => _login(context, ref),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Icon(Icons.arrow_forward_ios_rounded),
+                      ],
                     ),
                   ),
-                  Gap(40),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 7.5,
-                    children: [
-                      Container(
-                        width: 30,
-                        height: 2,
-                        color: Colors.black,
-                      ),
-                      Text(
-                        l10n.or_continue_with,
-                        style: textTheme.titleSmall,
-                      ),
-                      Container(
-                        width: 30,
-                        height: 2,
-                        color: Colors.black,
-                      )
-                    ],
-                  ),
-                  Gap(20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 20,
-                    children: [
-                      ClickableFrame(
-                        child: Placeholder(
-                          fallbackWidth: 56,
-                          fallbackHeight: 56,
-                        ),
-                      ),
-                      ClickableFrame(
-                        child: Placeholder(
-                          fallbackWidth: 56,
-                          fallbackHeight: 56,
-                        ),
-                      ),
-                      ClickableFrame(
-                        child: Placeholder(
-                          fallbackWidth: 56,
-                          fallbackHeight: 56,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Gap(20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        l10n.register_prompt,
-                        style: textTheme.titleSmall?.copyWith(
-                          fontWeight: FontWeight.w300
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          context.push('/register');
-                        },
-                        child: Text(
-                          l10n.sign_up,
-                          style: textTheme.titleSmall,
-                        )
-                      )
-                    ],
-                  )
-                ],
-              ),
+                ),
+              )
             ),
-          ),
+            Positioned(
+              top: 40,
+              left: 20,
+              child: ClickableFrame(
+                padding: const EdgeInsets.all(10),
+                onTap: () => AppSettingsDialog.show(context, ref),
+                child: Icon(Icons.settings)
+              )
+            ),
+          ],
         ),
       ),
     );
