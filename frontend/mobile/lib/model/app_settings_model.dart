525fc13be57d7403f085fe17fed17f4f365c775e
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-25 15:46:03 +0200

    feat(front>mobile>model>app_settings): Added function to save settings into SharedPreferences.

--- frontend/mobile/lib/model/app_settings_model.dart
+++ frontend/mobile/lib/model/app_settings_model.dart
@@ -1,3 +1,6 @@
+import 'package:area/core/constant/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class AppSettingsModel {
 
   String apiUrl;
@@ -9,5 +12,19 @@ class AppSettingsModel {
   });
 
   String get fullUrl => "$apiUrl:$apiPort";
+
+  Future<void> saveSettings({
+    required String apiUrl,
+    required String apiPort
+  }) async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+
+    print("Saving settings : $apiUrl, $apiPort");
+
+    this.apiUrl = apiUrl;
+    this.apiPort = apiPort;
+    sharedPreferences.setString(Constants.apiUrlKey, apiUrl);
+    sharedPreferences.setString(Constants.apiPortKey, apiPort);
+  }
 
 }
