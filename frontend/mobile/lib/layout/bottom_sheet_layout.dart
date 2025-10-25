2a8e6db99bff3c648219e090b65f31bf09481777
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-25 15:45:35 +0200

    feat(front>mobile>layout): Created `BottomSheetLayout` for bottom dialogs such as app settings.

---
+++ frontend/mobile/lib/layout/bottom_sheet_layout.dart
@@ -0,0 +1,120 @@
+import 'package:area/widget/dialog_button.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+
+class ModalBottomSheetLayout extends StatelessWidget {
+
+  final IconData icon;
+  final Color iconBackgroundColor;
+  final String title;
+  final String? description;
+  final bool showActions;
+  final bool Function()? onConfirm;
+  final List<Widget> children;
+
+  const ModalBottomSheetLayout({
+    super.key,
+
+    required this.icon,
+    this.iconBackgroundColor = Colors.grey,
+    required this.title,
+    this.description,
+    this.showActions = true,
+    this.onConfirm,
+    required this.children
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 12.5,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      color: iconBackgroundColor,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                          blurRadius: 4,
+                          color: iconBackgroundColor,
+                        )
+                      ]
+                    ),
+                    padding: const EdgeInsets.all(5),
+                    child: Icon(
+                      icon,
+                      color: Colors.white,
+                      size: 26,
+                    ),
+                  ),
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.titleMedium
+                    // ?.copyWith(
+                    //   color: Colors.white
+                    // ),
+                  ),
+                ],
+              ),
+
+              if (description != null) ... {
+                Gap(10),
+                Text(
+                  description!,
+                  style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                    fontWeight: FontWeight.normal
+                  )
+                )
+              },
+
+              Gap(20),
+              Column(
+                children: children
+              ),
+              Gap(20),
+
+              if (showActions) ... {
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 20,
+                  children: <Widget>[
+                    DialogButton(
+                      style: DialogButtonStyle.secondary,
+                      text: 'Cancel',
+                      onTap: () => context.pop()
+                    ),
+                    DialogButton(
+                      style: DialogButtonStyle.primary,
+                      text: 'Confirm',
+                      onTap: () async {
+                        if (onConfirm != null) {
+                          final dynamic result = await onConfirm!();
+
+                          if (result is bool && result == false) {
+                            return;
+                          }
+                        }
+
+                        context.pop();
+                      }
+                    ),
+                  ],
+                )
+              }
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+}
