c92a4b21ac9c5991ae9e6997337fd9e00f5454e8
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-25 15:48:02 +0200

    feat(front>mobile>data>provider>app_settings): Removed hard-coded keys.

--- frontend/mobile/lib/data/provider/app_settings_provider.dart
+++ frontend/mobile/lib/data/provider/app_settings_provider.dart
@@ -1,3 +1,4 @@
+import 'package:area/core/constant/constants.dart';
 import 'package:area/model/app_settings_model.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,8 +7,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 final appSettingsProvider = FutureProvider<AppSettingsModel>((ref) async {
   final sharedPreferences = await SharedPreferences.getInstance();
 
-  final apiUrl = sharedPreferences.getString('AREA_API_URL') ?? dotenv.env['API_URL']!;
-  final apiPort = sharedPreferences.getString('AREA_API_PORT') ?? dotenv.env['API_PORT']!;
+  final apiUrl = sharedPreferences.getString(Constants.apiUrlKey) ?? dotenv.env['API_URL']!;
+  final apiPort = sharedPreferences.getString(Constants.apiPortKey) ?? dotenv.env['API_PORT']!;
 
   return AppSettingsModel(
     apiUrl: apiUrl,
