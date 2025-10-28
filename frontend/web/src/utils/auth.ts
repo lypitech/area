c8074c5b5db77e799817353621c507d4a02c58cf
Babouye <esteban.bouyault-yvanez@epitech.eu>
2025-10-28 16:04:03 +0100

    feat(front>web>profile): Added edit handling in comment code for when it will be ready and adapt the related code

--- frontend/web/src/utils/auth.ts
+++ frontend/web/src/utils/auth.ts
@@ -1,8 +1,13 @@
 function clearLocalStorage() {
   localStorage.removeItem("access_token");
   localStorage.removeItem("refresh_token");
-  localStorage.removeItem("github_access_token");
+  clearAllServicesTokens();
   console.log("Local storage cleared");
+}
+
+function clearAllServicesTokens() {
+  localStorage.removeItem("github_access_token");
+  localStorage.removeItem("twitch_access_token");
 }
 
 function isLoggedIn() {
