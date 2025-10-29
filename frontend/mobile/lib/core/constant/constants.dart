f2769daa1c7e0d364edeee4f8e701847cb41922c
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-29 14:42:19 +0100

    feat(front>mobile>core>constants): Added keys as static variables to avoid hard-coding.

--- frontend/mobile/lib/core/constant/constants.dart
+++ frontend/mobile/lib/core/constant/constants.dart
@@ -6,7 +6,15 @@ class Constants {
 
   static const String oAuthUrlScheme = 'fr.lypitech.area';
   static const String oAuthBaseUrl = '$oAuthUrlScheme://oauth-callback';
+
+  static const String hiveCacheName = 'area_cache';
+
   static const String apiUrlKey = 'AREA_API_URL';
   static const String apiPortKey = 'AREA_API_PORT';
+
+  static const String envApiUrlKey = 'API_URL';
+  static const String envApiPortKey = 'API_PORT';
+  static const String envGithubClientIdKey = 'GITHUB_CLIENT_ID';
+  static const String envGithubClientSecretKey = 'GITHUB_CLIENT_SECRET';
 
 }
