4406cc9b511e36f487a4566e64970d1884bcd2d5
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-29 18:04:45 +0100

    style(front>mobile): Default environment variables check has been improved by a lot.

--- frontend/mobile/lib/main.dart
+++ frontend/mobile/lib/main.dart
@@ -3,24 +3,43 @@ import 'package:area/data/provider/hive_box_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'app.dart';
 
 Future<bool> initAppSettings() async {
-  if (!dotenv.env.containsKey('API_URL') || !dotenv.env.containsKey('API_PORT')) {
-    print('This build is broken. Default API URL and port are not specified.');
-    return false;
+  final requiredEnvVariables = [
+    Constants.envApiUrlKey,
+    Constants.envApiPortKey,
+    Constants.envGithubClientIdKey,
+    Constants.envGithubClientSecretKey
+  ];
+
+
+  for (String k in requiredEnvVariables) {
+    if (!dotenv.env.containsKey(k)) {
+      Fluttertoast.showToast(
+        msg: 'This build is broken. Default environment variables are not specified.'
+      );
+      return false;
+    }
   }
 
   final sharedPreferences = await SharedPreferences.getInstance();
 
   if (!sharedPreferences.containsKey(Constants.apiUrlKey)) {
-    sharedPreferences.setString(Constants.apiUrlKey, dotenv.env['API_URL']!);
+    sharedPreferences.setString(
+      Constants.apiUrlKey,
+      dotenv.env[Constants.envApiUrlKey]!
+    );
   }
 
   if (!sharedPreferences.containsKey(Constants.apiPortKey)) {
-    sharedPreferences.setString(Constants.apiPortKey, dotenv.env['API_PORT']!);
+    sharedPreferences.setString(
+      Constants.apiPortKey,
+      dotenv.env[Constants.envApiPortKey]!
+    );
   }
 
   return true;
@@ -36,7 +55,7 @@ void main() async {
   }
 
   await Hive.initFlutter();
-  final hiveBox = await Hive.openBox('area_cache');
+  final hiveBox = await Hive.openBox(Constants.hiveCacheName);
 
   runApp(
     ProviderScope(
