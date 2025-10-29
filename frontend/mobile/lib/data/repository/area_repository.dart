c772cf222a40394b176e35bd900f106ffe4317e7
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-29 17:54:54 +0100

    feat(front>mobile>data>repository): Created AREA repository. Things are still under work.

---
+++ frontend/mobile/lib/data/repository/area_repository.dart
@@ -0,0 +1,58 @@
+import 'package:area/api/area_api.dart';
+import 'package:area/model/area_model.dart';
+import 'package:area/model/user_model.dart';
+
+class AreaRepository {
+
+  final AreaApi api;
+
+  AreaRepository({
+    required this.api
+  });
+
+  Future<List<AreaModel>> fetchAreas({
+    required UserModel user
+  }) async {
+    final data = await api.getAreas(user.uuid);
+    return data.map((json) => AreaModel.fromJson(json)).toList();
+  }
+
+  Future<AreaModel> createArea({
+    required UserModel user,
+    required AreaModel area
+  }) async {
+    try {
+      final response = await api.createArea(
+        userUuid: user.uuid,
+        area: area
+      );
+
+      return AreaModel.fromJson(response);
+    } catch (_) {
+      return area.copyWith(isSyncing: true);
+    }
+  }
+
+  Future<void> deleteArea({
+    required UserModel user,
+    required String areaUuid
+  }) async {
+    await api.deleteArea(user.uuid, areaUuid);
+  }
+
+  Future<AreaModel> refreshArea({
+    required UserModel user,
+    required String areaUuid
+  }) async {
+    final data = await api.getArea(user.uuid, areaUuid);
+    final triggers = await api.getAreaTriggers(user.uuid, areaUuid);
+    final responses = await api.getAreaActions(user.uuid, areaUuid);
+
+    return AreaModel.fromJson({
+      ...data,
+      'triggers': triggers['triggers'] ?? [],
+      'responses': responses['responses'] ?? [],
+    });
+  }
+
+}
