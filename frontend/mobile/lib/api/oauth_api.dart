ae7ec3a84897cde366e6e5876b7945479fd1d782
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-10-31 20:07:17 +0100

    feat(front>mobile>api>oauth): Drafted `OAuthApi` class, with github oauth login for now.

---
+++ frontend/mobile/lib/api/oauth_api.dart
@@ -0,0 +1,30 @@
+import 'package:area/core/constant/constants.dart';
+import 'package:dio/dio.dart';
+
+class OauthApi {
+
+  final Dio dio;
+
+  OauthApi({
+    required this.dio,
+  });
+
+  Future<JsonData?> githubLogin({
+    required String userUuid,
+    required String code
+  }) async {
+    try {
+      final response = await dio.post(
+        '/oauth/github',
+        data: {
+          'uuid': userUuid,
+          'code': code
+        }
+      );
+      return response as JsonData;
+    } on DioException catch (exception) {
+      return exception.response?.data as JsonData?;
+    }
+  }
+
+}
