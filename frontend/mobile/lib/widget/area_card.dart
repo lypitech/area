13b402d5d96638c1a2816167f0729c3952460f3b
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-11-01 22:01:43 +0100

    feat(front>mobile>widget>area_card): Now shows platform images.

--- frontend/mobile/lib/widget/area_card.dart
+++ frontend/mobile/lib/widget/area_card.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
+import 'package:area/data/provider/platforms_icons_provider.dart';
 import 'package:area/model/area_model.dart';
 import 'package:area/widget/clickable_frame.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class AreaCard extends StatelessWidget {
+class AreaCard extends ConsumerWidget {
 
   final AreaModel area;
 
@@ -13,8 +17,12 @@ class AreaCard extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final textTheme = Theme.of(context).textTheme;
+    final iconsMap = ref.watch(platformsImageProvider);
+
+    final Image? triggerPlatformIcon = iconsMap[area.actionPlatform.name.toLowerCase()];
+    final Image? actionPlatformIcon = iconsMap[area.reactionPlatform.name.toLowerCase()];
 
     return ClickableFrame(
       onTap: () {
@@ -31,17 +39,35 @@ class AreaCard extends StatelessWidget {
           Row(
             spacing: 5,
             children: [
-              SizedBox(
+              Container(
                 height: 24,
                 width: 24,
-                child: Placeholder(),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+                child: triggerPlatformIcon
+                  ?? Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      shape: BoxShape.circle
+                    ),
+                  ),
               ),
               Icon(Icons.link_rounded),
-              SizedBox(
+              Container(
                 height: 24,
                 width: 24,
-                child: Placeholder(),
-              )
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+                child: actionPlatformIcon
+                  ?? Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      shape: BoxShape.circle
+                    ),
+                  ),
+              ),
             ],
           ),
           Text(
