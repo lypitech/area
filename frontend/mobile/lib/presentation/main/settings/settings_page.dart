66843668643cbdb982a11083e0db327356a9fdbf
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-11-02 00:05:45 +0100

    feat(front>mobile>presentation>settings_page): Added account deletion button, and added icon for logout button.

--- frontend/mobile/lib/presentation/main/settings/settings_page.dart
+++ frontend/mobile/lib/presentation/main/settings/settings_page.dart
@@ -3,6 +3,8 @@ import 'package:area/layout/main_page_layout.dart';
 import 'package:area/model/settings_tile_model.dart';
 import 'package:area/model/user_model.dart';
 import 'package:area/presentation/dialog/app_settings_dialog.dart';
+import 'package:area/presentation/dialog/error_dialog.dart';
+import 'package:area/widget/a_popup.dart';
 import 'package:area/widget/appbar_button.dart';
 import 'package:area/widget/clickable_frame.dart';
 import 'package:area/widget/settings_tile.dart';
@@ -42,6 +44,8 @@ class SettingsPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final textTheme = Theme.of(context).textTheme;
+
     return MainPageLayout(
       title: 'Settings',
       leading: AppbarButton(
@@ -79,15 +83,85 @@ class SettingsPage extends ConsumerWidget {
           color: Colors.red,
           onTap: () => authLogout(context, ref),
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          child: Center(
-            child: Text(
-              'Log out',
-              style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                color: Colors.white
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 5,
+            children: [
+              Icon(
+                Icons.logout_rounded,
+                color: Colors.white,
               ),
-            )
+              Text(
+                'Log out',
+                style: textTheme.titleMedium?.copyWith(
+                  color: Colors.white
+                ),
+              )
+            ],
           )
-        )
+        ),
+        Divider(
+          color: Colors.grey.shade300,
+        ),
+        Text(
+          '⚠️ Danger zone',
+          style: textTheme.titleMedium?.copyWith(
+            color: Colors.red,
+            fontSize: 22
+          ),
+        ),
+        ClickableFrame(
+          color: Colors.red,
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context1) {
+                return APopup(
+                  title: 'Account deletion',
+                  icon: Icons.delete_rounded,
+                  iconBackgroundColor: Colors.red,
+                  content: 'Are you sure you want to delete your account?\nThis action cannot be undone!',
+                  confirmButtonColor: Colors.red,
+                  onConfirm: () async {
+                    try {
+                      final authNotifier = await ref.read(authNotifierProvider.future);
+                      await authNotifier.deleteAccount();
+
+                      if (context.mounted) {
+                        context.pop();
+                        context.go('/login');
+                      }
+                    } catch (e) {
+                      if (context.mounted) {
+                        ErrorDialog.show(
+                          context: context,
+                          error: 'There has been a problem while deleting your account.\n($e)'
+                        );
+                      }
+                    }
+                  },
+                );
+              }
+            );
+          },
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 5,
+            children: [
+              Icon(
+                Icons.delete_rounded,
+                color: Colors.white,
+              ),
+              Text(
+                'Delete your account',
+                style: textTheme.titleMedium?.copyWith(
+                  color: Colors.white
+                ),
+              )
+            ]
+          )
+        ),
       ]
     );
   }
