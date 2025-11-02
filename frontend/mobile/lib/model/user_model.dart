8d7931585d44b32293ab94e949470a28819d1471
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-11-02 14:55:26 +0100

    feat(front>mobile>model>user): Now parses profile picture if there is one.

--- frontend/mobile/lib/model/user_model.dart
+++ frontend/mobile/lib/model/user_model.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:area/core/constant/constants.dart';
+import 'package:flutter/material.dart';
 
 class UserModel {
 
@@ -7,29 +10,39 @@ class UserModel {
   final String nickname;
   final String email;
   final Map<String, String> oauthUuids;
+  final Image? profilePictureBase64;
 
   const UserModel({
     required this.uuid,
     required this.username,
     required this.nickname,
     required this.email,
-    required this.oauthUuids
+    required this.oauthUuids,
+    this.profilePictureBase64
   });
 
   factory UserModel.fromJson(JsonData data) {
+    print("FROMJSON CALLED");
     final oauthList = data['oauth_uuids'] as List? ?? [];
     final oauthUuids = {
       for (final e in oauthList)
         if (e is Map && e['service_name'] != null && e['token_uuid'] != null)
           e['service_name'] as String: e['token_uuid'] as String
     };
+    print("PARSED OAUTHLIST");
 
+    final rawImageBase64 = data['profilePicture'];
+
+    print("PARSED RAWIMAGEBASE64: $rawImageBase64");
     return UserModel(
       uuid: data['uuid'],
-      username: data['username'],
+      username: data['username'] as String? ?? '',
       nickname: data['nickname'],
       email: data['email'],
-      oauthUuids: oauthUuids
+      oauthUuids: oauthUuids,
+      profilePictureBase64: rawImageBase64 != null
+        ? Image.memory(base64Decode(rawImageBase64))
+        : null
     );
   }
 
