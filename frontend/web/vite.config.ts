87e81801de91c6abb06b6a0a12b5fc3b37834e69
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-11-02 20:25:27 +0100

    feat(front>web): Updated Vite config.

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 8081
-  }
+  },
+  preview: { host: true, port: 8081, strictPort: true },
+  publicDir: resolve(__dirname, 'public'),
 })
